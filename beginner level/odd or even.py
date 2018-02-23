089fb2963cfad4d0aeed503292c4b25e3dd4beff
muthueee <36726608+muthueee@users.noreply.github.com>
2018-02-23 14:48:58 +0530

    Update odd or even.py

--- beginner level/odd or even.py
+++ beginner level/odd or even.py
@@ -1,5 +1,8 @@
 num = int(input("Enter a number: "))
-if (num % 2) == 0:
+for n in range(10000):
+ if (num % 2) == 0:
    print("{0} is Even".format(num))
-else:
+   break
+ else:
    print("{0} is Odd".format(num))
+   break
