6409420bf1e2565e1984d72ebd613f6c9a28dd00
muthueee <36726608+muthueee@users.noreply.github.com>
2018-02-23 14:56:22 +0530

    Delete odd or even.py

--- beginner level/odd or even.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number: "))
-for n in range(10000):
- if (num % 2) == 0:
-   print("{0} is Even".format(num))
-   break
- else:
-   print("{0} is Odd".format(num))
-   break
