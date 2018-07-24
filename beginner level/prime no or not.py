64b78736fb31993dcefcdf91f819ced0ee7e4056
muthueee <36726608+muthueee@users.noreply.github.com>
2018-07-24 14:50:38 +0530

    Update prime no or not.py

--- beginner level/prime no or not.py
+++ beginner level/prime no or not.py
@@ -1,8 +1,8 @@
 n = int(input())
-for i in range(1,1111):
-	if i/i:
-		print("it is prime no.")
+for i in range(2,1001):
+	if n%i==0:
+		print("it is not prime no.")
 		break
 	else:
-		print("it is not prime no.")
+		print("it is prime no.")
 		break
